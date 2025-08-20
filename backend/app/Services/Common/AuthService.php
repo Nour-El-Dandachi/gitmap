33ca099058f05fdd539dac44e5fc9453095335e6
Nour El Dandachi <152388195+nourdandachi@users.noreply.github.com>
2025-08-20 22:52:05 +0300

    created register service

--- backend/app/Services/Common/AuthService.php
+++ backend/app/Services/Common/AuthService.php
@@ -26,4 +26,24 @@ class AuthService
         $user->token = $token;
         return $user;
     }
+    
+    public static function register(Request $request){
+        $request->validate([
+            "name" => "required|string|max:255",
+            "email" => "required|string|email|max:255|unique:users",
+            "password" => "required|string|min:6",
+        ]);
+
+        $user = new User;
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = Hash::make($request->password);
+        $user->save();
+
+        $token = Auth::login($user);
+
+        $user->token = $token;
+        return $user;
+    }
+
 }
