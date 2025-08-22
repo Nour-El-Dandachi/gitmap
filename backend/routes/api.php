1d3d9f4af46abc905e8c6fe6cf180b704799a6c6
Nour El Dandachi <152388195+nourdandachi@users.noreply.github.com>
2025-08-22 20:40:45 +0300

    created login/register routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,12 +2,14 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Common\AuthController;
 use App\Http\Controllers\Common\GithubAuthController;
 
-Route::group(["prefix" =>"v0.1"], function(){
-    
+Route::group(["prefix" => "v0.1"], function () {
 
-    Route::group(["prefix" => "guest"], function(){
-       
+
+    Route::group(["prefix" => "guest"], function () {
+        Route::post("/login", [AuthController::class, "login"]);
+        Route::post("/register", [AuthController::class, "register"]);
     });
 });
