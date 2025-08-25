7e2b4125164c03f993999649d3fe44ff78ed3955
Nour El Dandachi <152388195+nourdandachi@users.noreply.github.com>
2025-08-25 14:31:35 +0300

    created fetch tree function

--- backend/app/Http/Controllers/User/RepoController.php
+++ backend/app/Http/Controllers/User/RepoController.php
@@ -5,11 +5,25 @@ namespace App\Http\Controllers\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
+use App\Services\User\RepoService;
 
 class RepoController extends Controller{
 
-    public function addOrUpdateTree(){
+    public function fetchTree(Request $request){
         
+        $url = $request->input('url');
+
+        if(!$url){
+            return $this->responseJSON(null, 'Reposiory URL is required', 400);
+        }
+
+        $tree = RepoService::fetchTree($url);
+
+        if(!$tree){
+            return $this->responseJSON(null, 'Failed to fetch repo tree', 500);
+        }
+
+        return $this->responseJSON($tree);
     }
     
 }
