684bf26997a496f9d82d7e13d032aeceec056aa8
Nour El Dandachi <152388195+nourdandachi@users.noreply.github.com>
2025-08-26 21:26:08 +0300

    added icon to input component

--- frontend/src/components/shared/input/index.jsx
+++ frontend/src/components/shared/input/index.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import { Mail, Lock } from "lucide-react";
 import "./input.css";
 
-const Input = ({ type, name, hint, onChange, value }) => {
+const Input = ({ type, name, hint, onChange, icon, value }) => {
+  const setIcon = () => {
+    if (icon === "lock") {
+      return <Lock className="icon" color="#131325" />;
+    } else if (icon === "mail") {
+      return <Mail className="icon" color="#131325" />;
+    }
+  };
   return (
     <div className="input-wrapper">
+      {setIcon()}
       <input
         type={type}
         name={name}
