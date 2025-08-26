09f545aa93ca359c0e2d31004b91c3fed900863c
Nour El Dandachi <152388195+nourdandachi@users.noreply.github.com>
2025-08-26 22:18:30 +0300

    created login right side component

---
+++ frontend/src/components/login/left-side/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './left-side.css';
+
+const LeftSide = () => {
+    return (
+        <div>
+            LeftSide
+        </div>
+    );
+}
+
+export default LeftSide;
