f730fbe5ac20cc456ca882de5ec16e62e1480158
Nour El Dandachi <152388195+nourdandachi@users.noreply.github.com>
2025-08-26 21:13:52 +0300

    created input component

---
+++ frontend/src/components/shared/input/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './input.css';
+
+const Input = () => {
+    return (
+        <div>
+            Input component
+        </div>
+    );
+}
+
+export default Input;
