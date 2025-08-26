87f6ef1ec134c14c15675df5fcc9fcc5056be153
Nour El Dandachi <152388195+nourdandachi@users.noreply.github.com>
2025-08-26 20:41:13 +0300

    added login route

--- frontend/src/routes/MyRoutes.jsx
+++ frontend/src/routes/MyRoutes.jsx
@@ -1,11 +1,13 @@
-import React from "react";
-import {Routes,Route} from "react-router-dom";
+import React from 'react';
+import Login from '../pages/login/index.jsx'
+import {Routes,Route} from 'react-router-dom';
+
 
 const MyRoutes = ()=>{
 
     return (
         <Routes>
-            
+            <Route path='/login' element={<Login />}/>
         </Routes>
     );
 }
