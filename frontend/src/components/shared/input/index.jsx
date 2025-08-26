e7866fa312f97329f80345815304c1bba7defa57
Nour El Dandachi <152388195+nourdandachi@users.noreply.github.com>
2025-08-26 21:23:34 +0300

    applied prettier

--- frontend/src/components/shared/input/index.jsx
+++ frontend/src/components/shared/input/index.jsx
@@ -1,12 +1,18 @@
 import React from "react";
-import './input.css';
+import "./input.css";
 
 const Input = ({ type, name, hint, onChange, value }) => {
   return (
     <div className="input-wrapper">
-      <input type={type} name={name} placeholder={hint} onChange={onChange} value={value}/>
+      <input
+        type={type}
+        name={name}
+        placeholder={hint}
+        onChange={onChange}
+        value={value}
+      />
     </div>
   );
-}
+};
 
 export default Input;
