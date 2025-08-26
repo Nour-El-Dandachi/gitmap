23717e979c91f7af9dae3cff0c5605a3ed362d93
Nour El Dandachi <152388195+nourdandachi@users.noreply.github.com>
2025-08-26 21:21:57 +0300

    made input field

--- frontend/src/components/shared/input/index.jsx
+++ frontend/src/components/shared/input/index.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import './input.css';
 
-const Input = () => {
-    return (
-        <div>
-            Input component
-        </div>
-    );
+const Input = ({ type, name, hint, onChange, value }) => {
+  return (
+    <div className="input-wrapper">
+      <input type={type} name={name} placeholder={hint} onChange={onChange} value={value}/>
+    </div>
+  );
 }
 
 export default Input;
