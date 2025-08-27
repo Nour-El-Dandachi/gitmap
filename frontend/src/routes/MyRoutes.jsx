2c9c73abcf19023600f359b9e98572049e2c5d61
Nour El Dandachi <152388195+nourdandachi@users.noreply.github.com>
2025-08-27 12:56:58 +0300

    added home route

--- frontend/src/routes/MyRoutes.jsx
+++ frontend/src/routes/MyRoutes.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Login from '../pages/login/index.jsx'
 import Register from '../pages/register/index.jsx'
+import Home from '../pages/home/index.jsx';
 import {Routes,Route} from 'react-router-dom';
 
 
@@ -10,6 +11,7 @@ const MyRoutes = ()=>{
         <Routes>
             <Route path='/login' element={<Login />}/>
             <Route path='/register' element={<Register />}/>
+            <Route path='/home' element={<Home />}/>
         </Routes>
     );
 }
