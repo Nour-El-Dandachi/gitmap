4b1c09b30d6111f78378064aba5b7b62faff2aa7
Nour El Dandachi <152388195+nourdandachi@users.noreply.github.com>
2025-08-27 11:19:50 +0300

    added logo to the btn

--- frontend/src/components/shared/github-btn/index.jsx
+++ frontend/src/components/shared/github-btn/index.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import "./github-btn.css";
+import githubLogo from "../../../assets/logos/github-logo.png";
 
 const GithubButton = ({ location }) => {
   return (
     <div className="github-btn-wrapper">
-      <div className="github-img"> <img src="../../../../public/logos/github-logo.png"></img></div>
+      <div className="github-img">
+        <img src={githubLogo} alt="GitHub Logo" />
+      </div>
       <div className="github-btn-text">
         {location === "login" ? "Continue with GitHub" : "Sign up with GitHub"}
       </div>
