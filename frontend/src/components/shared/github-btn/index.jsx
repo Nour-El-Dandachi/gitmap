d9742ec1b55810ab8302d581b48b190f92831a65
Nour El Dandachi <152388195+nourdandachi@users.noreply.github.com>
2025-08-27 10:59:04 +0300

    created github btn component

---
+++ frontend/src/components/shared/github-btn/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './github-btn.css';
+
+const GithubButton = () => {
+    return (
+        <div>
+            GithubButton
+        </div>
+    );
+}
+
+export default GithubButton;
