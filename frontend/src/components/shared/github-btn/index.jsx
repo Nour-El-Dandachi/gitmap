767c0d663b433021728b8c9dcfd5209756de54db
Nour El Dandachi <152388195+nourdandachi@users.noreply.github.com>
2025-08-27 11:15:28 +0300

    added logos

--- frontend/src/components/shared/github-btn/index.jsx
+++ frontend/src/components/shared/github-btn/index.jsx
@@ -4,7 +4,7 @@ import "./github-btn.css";
 const GithubButton = ({ location }) => {
   return (
     <div className="github-btn-wrapper">
-      <div className="github-img"></div>
+      <div className="github-img"> <img src="../../../../public/logos/github-logo.png"></img></div>
       <div className="github-btn-text">
         {location === "login" ? "Continue with GitHub" : "Sign up with GitHub"}
       </div>
