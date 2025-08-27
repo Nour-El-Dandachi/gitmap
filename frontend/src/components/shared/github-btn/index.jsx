ac1ee1a56f3665babc5ff69ffe5b67d9766412be
Nour El Dandachi <152388195+nourdandachi@users.noreply.github.com>
2025-08-27 11:48:07 +0300

    added redirect function

--- frontend/src/components/shared/github-btn/index.jsx
+++ frontend/src/components/shared/github-btn/index.jsx
@@ -3,15 +3,19 @@ import "./github-btn.css";
 import githubLogo from "../../../assets/logos/github-logo.png";
 
 const GithubButton = ({ location }) => {
+  const handleClick = () => {
+    window.location.href = "http://localhost:8000/auth/github/redirect";
+  };
+
   return (
-    <div className="github-btn-wrapper">
-      <div >
+    <button type="button" className="github-btn-wrapper" onClick={handleClick}>
+      <div>
         <img className="github-img" src={githubLogo} alt="GitHub Logo" />
       </div>
       <div className="github-btn-text">
         {location === "login" ? "Continue with GitHub" : "Sign up with GitHub"}
       </div>
-    </div>
+    </button>
   );
 };
 
