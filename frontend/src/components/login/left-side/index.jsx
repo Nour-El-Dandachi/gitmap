be45108986b75ffa9efc8b4d8016ac9815ab065d
Nour El Dandachi <152388195+nourdandachi@users.noreply.github.com>
2025-08-27 15:33:07 +0300

    added divider

--- frontend/src/components/login/left-side/index.jsx
+++ frontend/src/components/login/left-side/index.jsx
@@ -1,20 +1,21 @@
 import React from "react";
 import GithubButton from "../../shared/github-btn";
-import './left-side.css';
-
+import "./left-side.css";
 
 const LeftSide = () => {
-    return (
-        <div className="login-left-side">
-            <div className="login-logo">
-                
-            </div>
-            <div className="login-form">
-                <h1>Welome back to gitmap!</h1>
-                <GithubButton location={"login"} />
-            </div>
+  return (
+    <div className="login-left-side">
+      <div className="login-logo"></div>
+      <div className="login-form">
+        <h1>Welome back to gitmap!</h1>
+        <GithubButton location={"login"} />
+        <div className="divider">
+          <span>OR</span>
         </div>
-    );
-}
+        
+      </div>
+    </div>
+  );
+};
 
 export default LeftSide;
