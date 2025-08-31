b671105e195a2d70ff4af3776dc67f38098bd8ab
Nour El Dandachi <152388195+nourdandachi@users.noreply.github.com>
2025-08-31 13:04:24 +0300

    created notifications model

--- backend/notifications/models.py
+++ backend/notifications/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from users.models import User
 
-# Create your models here.
+class Notification(models.Model):
+    id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    message = models.TextField()
+    is_read = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
