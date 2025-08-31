e88033bbf88eae41d05bb0fae50bedb28562c7e5
Nour El Dandachi <152388195+nourdandachi@users.noreply.github.com>
2025-08-31 12:22:51 +0300

    created super user

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import User
 
-# Register your models here.
+@admin.register(User)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'email', 'role', 'github_login')
+    search_fields = ('name', 'email', 'github_login')
+    list_filter = ('role',)
