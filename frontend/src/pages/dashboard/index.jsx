fd49b47ba44fc2e4cd71abaf16db770523c72bf5
Nour-El-Dandachi <nourdandachi18@gmail.com>
2025-09-04 09:59:29 +0300

    frontend/created header

--- frontend/src/pages/dashboard/index.jsx
+++ frontend/src/pages/dashboard/index.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import './dashboard.css';
 import SideBar from "../../components/shared/side-bar";
+import Header from "../../components/shared/header";
 
 const Dashboard = () => {
     return (
-        <div>
+        <div className="dashboard-page">
             <SideBar name={"Nour El Dandachi"}/>
+            <Header location={"Dashboard"} />
         </div>
     );
 }
