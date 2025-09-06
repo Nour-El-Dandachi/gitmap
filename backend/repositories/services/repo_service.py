091d213af9824a69534c0885bcc65555b99230fa
Nour-El-Dandachi <nourdandachi18@gmail.com>
2025-09-06 12:15:56 +0300

    backend/added repo files to the db

--- backend/repositories/services/repo_service.py
+++ backend/repositories/services/repo_service.py
@@ -1,9 +1,14 @@
+#/services/repo_service.py
+
 import requests
 from urllib.parse import urlparse
 import base64
-from repositories.models import Repository
+import os 
+
+from repositories.models import Repository, RepoFile
 from indexing.models import IndexingJob
 from django.utils import timezone
+
 
 GITHUB_API_BASE = "https://api.github.com"
 
@@ -11,6 +16,12 @@ HEADERS = {
     "Accept": "application/vnd.github.v3+json",
     "User-Agent": "GitMap"
 }
+
+ACCEPTED_EXTENSIONS = [
+        ".py", ".js", ".ts", ".jsx", ".tsx",
+        ".html", ".css", ".scss", ".java", ".php",
+        ".rb", ".go", ".cpp", ".c", ".cs", ".json", ".yml", ".yaml", ".md"
+]
 
 class RepoService:
 
@@ -99,5 +110,56 @@ class RepoService:
             repository=repo,
             status="pending"
         )
+        RepoService.save_repo_tree(repo)
+        repo.index_status = "tree_saved"
+        repo.save()
 
         return repo
+
+
+
+    @staticmethod
+    def save_repo_tree(repo: Repository):
+
+        owner = repo.owner
+        name = repo.name
+        branch = repo.default_branch
+
+        tree_url = f"{GITHUB_API_BASE}/repos/{owner}/{name}/git/trees/{branch}?recursive=1"
+        response = requests.get(tree_url, headers=HEADERS)
+        if response.status_code != 200:
+            raise Exception("Failed to fetch GitHub tree")
+
+        tree = response.json().get("tree", [])
+
+        for item in tree:
+            if item["type"] != "blob":
+                continue 
+
+            path = item["path"]
+            sha = item["sha"]
+            size = item.get("size", 0)
+            file_name = os.path.basename(path)
+            parent_path = os.path.dirname(path)
+            extension = os.path.splitext(file_name)[1].lower()
+
+            if extension not in ACCEPTED_EXTENSIONS or size > 200_000: 
+                continue
+
+            RepoFile.objects.create(
+                repository=repo,
+                path=path,
+                file_name=file_name,
+                parent_path=parent_path,
+                extension=extension,
+                type="code",
+                sha=sha,
+                size=size,
+                is_binary=False,
+                is_indexed=False
+            )
+
+        repo.index_status = "tree_saved"
+        repo.is_indexed = False
+        repo.save()
+
