e2591af524eed959c9c3b06659f719fa8c57b1a0
Nour-El-Dandachi <nourdandachi18@gmail.com>
2025-09-10 13:23:53 +0300

    frontend/adjusted logo in side bar

--- frontend/src/components/shared/side-bar/index.jsx
+++ frontend/src/components/shared/side-bar/index.jsx
@@ -1,17 +1,21 @@
 import React from "react";
 import "./side-bar.css";
 import { Bell, Github, LayoutDashboard, Search, ChevronDown, UserRound } from "lucide-react";
+import gitmapLogo from "../../../assets/logos/gitmap_3.png";
 
 const SideBar = ({name}) => {
   return (
     <div className="side-bar">
+      <div className="header-img">
+        <img src={gitmapLogo} alt="logo" className="logo" />
+      </div>
       <div className="bar-profile">
         <div className="profile-photo"><UserRound /></div>
         <div className="profile-name"> {name} <ChevronDown /></div>
       </div>
-      <div className="bar-search">
+      {/* <div className="bar-search">
         <Search /> <input className="search-input" placeholder="Search for anything" />
-      </div>
+      </div> */}
       <div className="bar-menu">
             <div className="menu-dashboard"><LayoutDashboard/> Dashboard</div>
             <div className="menu-repositories"><Github/> My Repositories</div>
