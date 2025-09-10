fca8ffab2948f8f8539d64a0d323791a5dce069f
Nour-El-Dandachi <nourdandachi18@gmail.com>
2025-09-10 15:32:16 +0300

    frontend/created notification route and component

---
+++ frontend/src/components/notification-card/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './notification-card.css';
+
+const NotificationCard = () => {
+    return (
+    <div className="notification-card">
+     
+    </div>
+  );
+};
+
+
+export default NotificationCard;
---
+++ frontend/src/pages/notifications/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './notifications.css';
+
+const Notifications= () => {
+    return (
+    <div className="notifications-page">
+     
+    </div>
+  );
+};
+
+
+export default Notifications;
