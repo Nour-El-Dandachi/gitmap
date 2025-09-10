b8cd546bb47d67a1f1789abfef37985432580d07
Nour-El-Dandachi <nourdandachi18@gmail.com>
2025-09-10 15:34:41 +0300

    frontend/added side bar and header to notifications page

--- frontend/src/pages/notifications/index.jsx
+++ frontend/src/pages/notifications/index.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import './notifications.css';
+import SideBar from "../../components/shared/side-bar";
+import Header from "../../components/shared/header";
+import NotificationCard from "../../components/notification-card";
 
 const Notifications= () => {
     return (
-    <div className="notifications-page">
-     
+    <div className="notifications-page">=
+      <SideBar name={"Nour El Dandachi"} />
+      <div className="dashboard-right">
+        <Header location={"Notifications"} />
+
+      </div>
     </div>
   );
 };
