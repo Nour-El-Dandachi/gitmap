70771798f9a1e2994ff949d0f60005815d5c9b83
Nour-El-Dandachi <nourdandachi18@gmail.com>
2025-09-10 15:41:17 +0300

    frontend/added search bar

--- frontend/src/pages/notifications/index.jsx
+++ frontend/src/pages/notifications/index.jsx
@@ -2,16 +2,22 @@ import React from "react";
 import './notifications.css';
 import SideBar from "../../components/shared/side-bar";
 import Header from "../../components/shared/header";
+import { Search } from "lucide-react";
 import NotificationCard from "../../components/notification-card";
 
 const Notifications= () => {
     return (
-    <div className="notifications-page">=
+    <div className="notifications-page">
       <SideBar name={"Nour El Dandachi"} />
-      <div className="dashboard-right">
+      <div className="notifications-right">
         <Header location={"Notifications"} />
-
+        <div className="repo-search">
+          <Search />{" "}
+          <input className="search-input" placeholder="Search for anything" />
+          
+        </div>
       </div>
+      
     </div>
   );
 };
