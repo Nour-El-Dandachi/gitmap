8b7fff6cf84894e11cc783c3fd96d739536e75ad
Nour-El-Dandachi <nourdandachi18@gmail.com>
2025-09-17 14:10:11 +0300

    backend/updated ci

--- backend/repositories/tasks.py
+++ backend/repositories/tasks.py
@@ -4,8 +4,11 @@ from celery import shared_task
 from repositories.models import Repository
 from repositories.services.embedding_service import EmbeddingService
 from sentence_transformers import SentenceTransformer
+import os
 
-model = SentenceTransformer("/app/bge-m3")
+model = None
+if not os.environ.get("CI"):
+    model = SentenceTransformer("/app/bge-m3")
 
 def embed_local(text):
     return model.encode(text).tolist()
